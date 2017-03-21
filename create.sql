04680189d7d8025211df828802aabbcaaed05c60
flipperswitch <flipperswitch@gmail.com>
2017-03-21 18:08:45 -0400

    Create now runs without bugs but still needs tweaking

--- create.sql
+++ create.sql
@@ -1,59 +1,62 @@
 -- Create Tables
 create table position(
-    pos_name char(20) not null primary key,
-    pay_rate decimal(4,2) not null,
-    check (pos_name in ('Dispatcher', 'Senior Dispatcher', 'Head Dispatcher', 'Parking Enforcement Assistant',
-         'Parking Attendant', 'Opener', 'Closer')));
+    pos_name varchar(20) not null primary key,
+    pay_rate decimal(4,2) not null);
 
 create table worker(
     gid integer not null primary key,
-    first char(20) not null,
-    last char(20) not null);
+    first varchar(20) not null,
+    last varchar(20) not null);
 
 create table works(
-    pos_name char(20) not null references position,
-    gid integer not null references worker,
+    pos_name varchar(20) not null references position(pos_name),
+    gid integer not null references worker(gid),
     primary key(pos_name, gid));
 
 create table shift(
     sid varchar(16) not null primary key,
     day char (10) not null,
-    start_time integer(4) not null,
-    end_time integer(4) not null,
-    term, char(15) not null,
-    is_rotation boolean not null,
-    gid integer references worker,
-    check(day in ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')),
-    check(term in ('Spring', 'Fall', 'Summer', 'Winter Break', 'Spring Break', 'Thanksgiving')));
+    start_time integer not null,
+    end_time integer not null,
+    term varchar(15) not null,
+    is_rotation varchar(1) not null,
+    gid integer references worker(gid),
+    check(day in ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+        'Saturday', 'Sunday')),
+    check(term in ('Spring', 'Fall', 'Summer', 'Winter Break', 'Spring Break',
+        'Thanksgiving')),
+    check(is_rotation in ('T', 'F')));
 
 create table shift_type(
-    sid varchar(16) not null references shift,
-    pos_name char(20) not null references position,
+    sid varchar(16) not null references shift(sid),
+    pos_name char(20) not null references position(pos_name),
     primary key(sid, pos_name));
 
 create table rotation_assigned(
-    sid varchar(16) not null references shift,
-    s_date date not null,
-    gid integer references worker,
-    primary key(sid, c_date));
+    sid varchar(16) not null references shift(sid),
+    a_date date not null,
+    gid integer references worker(gid),
+    primary key(sid, a_date));
 
 create table coverage(
-    sid varchar(16) not null references shift,
+    sid varchar(16) not null references shift(sid),
     c_date date not null,
-    gid integer references worker,
-    mandatory boolean not null,
-    primary key(sid, c_date));
+    gid integer references worker(gid),
+    mandatory char(1) not null,
+    primary key(sid, c_date),
+    check(mandatory in('T', 'F')));
 
 create table parker_shift(
     p_date date not null,
-    start_time integer(4) not null,
-    end_time integer(4) not null,
+    start_time integer not null,
+    end_time integer not null,
     num_req integer not null,
     primary key (p_date, start_time, end_time));
 
 create table parker_assigned(
-    p_date date not null references parker_shift,
-    start_time integer(4) not null references parker_shift,
-    end_time integer(4) not null references parker_shift,
-    gid integer references worker,
-    primary key (p_date, start_time, end_time, gid));
+    p_date date not null,
+    start_time integer not null,
+    end_time integer not null,
+    gid integer not null references worker(gid),
+    primary key (p_date, start_time, end_time, gid),
+    foreign key (p_date, start_time, end_time) references parker_shift);
