30f080655320c6d9694d38df0ec621c3186298ab
annv12321 <anna.pelletier@gordon.edu>
2017-03-21 23:17:14 -0400

    Update test_coverage.sql

--- test/test_coverage.sql
+++ test/test_coverage.sql
@@ -1,8 +1,8 @@
--- duplicate primary key
+-- testing with duplicate primary key in Coverage table
 insert into coverage values('DISP17MD00000100', '2017-04-03', '00000000', 'F');
--- first foreign key does not exist
+-- testing with first foreign key does not exist in Coverage table
 insert into coverage values('AAAAAAAAAAAAAAAA', '2017-04-03', '00000000', 'F');
--- second foreign key does not exist
+-- testing with second foreign key does not exist in Coverage table
 insert into coverage values('DISP17TU00000100', '2017-04-03', '99999999', 'F');
--- improper boolean value
+-- testing with improper boolean value in Coverage table
 insert into coverage values('DISP17TU00000100', '2017-04-03', '00000000', 'N');
