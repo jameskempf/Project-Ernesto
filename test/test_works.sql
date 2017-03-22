1c04bb099b15e1c5e34164b4c816acec0da5d580
annv12321 <anna.pelletier@gordon.edu>
2017-03-22 00:24:12 -0400

    Update test_works.sql

--- test/test_works.sql
+++ test/test_works.sql
@@ -4,3 +4,11 @@ insert into works values('test', '00000000');
 insert into works values('test1', '00000000');
 --  testing with second foreign key does not exist in Works table
 insert into works values('test', '00000009');
+-- testing the deleting cascade in Parker Assigned table (pos_name)
+select * from works;
+delete * from position where pos_name = 'Parker';
+select * from works;
+-- testing the deleting cascade in Parker Assigned table (gid)
+select * from works;
+delete * from worker where gid = '44444444';
+select * from works;
