484e969115a6998b606b634d2ae99bfdc9a60ab4
annv12321 <anna.pelletier@gordon.edu>
2017-03-22 00:28:49 -0400

    Update test_shift_type.sql

--- test/test_shift_type.sql
+++ test/test_shift_type.sql
@@ -4,3 +4,11 @@ insert into shift_type values('DISP17MD00000100', 'Dispatcher');
 insert into shift_type values('AAAAAAAAAAAAAAAA', 'Dispatcher');
 --  testing with second foreign key does not exist in Shift Type table
 insert into shift_type values('DISP17MD00000100', 'not a pos_name');
+-- testing the deleting cascade in Parker Assigned table (gid)
+select * from shift_type;
+delete * from shift where sid = 'AAAAAAAAAAAAAAAA';
+select * from shift_type;
+-- testing the deleting cascade in Parker Assigned table (gid)
+select * from shift_type;
+delete * from position where pos_name = 'Senior Dispatcher';
+select * from shift_type;
