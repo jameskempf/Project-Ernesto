32b97104c28cdded53f0200349908760b48289e6
annv12321 <anna.pelletier@gordon.edu>
2017-03-22 00:32:10 -0400

    Update test_rotation_assigned.sql

--- test/test_rotation_assigned.sql
+++ test/test_rotation_assigned.sql
@@ -4,3 +4,11 @@ insert into rotation_assigned values('DISP17MD00000100', '2017-04-07', '00000000
 insert into rotation_assigned values('AAAAAAAAAAAAAAAA', '2017-04-07', '00000000');
 --  testing with second foreign key does not exist in Rotation Assigned table
 insert into rotation_assigned values('DISP17TU00000100', '2017-04-07', '99999999');
+-- testing the deleting cascade in Rotation Assigned table (gid)
+select * from rotation_assigned;
+delete * from shift where sid = 'AAAAAAAAAAAAAAAA';
+select * from rotation_assigned;
+-- testing the deleting cascade in Rotation Assigned table (gid)
+select * from rotation_assigned;
+delete * from worker where gid = '12345678';
+select * from rotation_assigned;
