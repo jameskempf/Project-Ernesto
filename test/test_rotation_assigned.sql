d5cba397f639a86e0889203061f71870fb307930
annv12321 <anna.pelletier@gordon.edu>
2017-03-21 23:20:19 -0400

    Update test_rotation_assigned.sql

--- test/test_rotation_assigned.sql
+++ test/test_rotation_assigned.sql
@@ -1,6 +1,6 @@
--- duplicate primary key
+--  testing with duplicate primary key in Rotation Assigned table
 insert into rotation_assigned values('DISP17MD00000100', '2017-04-07', '00000000');
--- first foreign key does not exist
+--  testing with first foreign key does not exist in Rotation Assigned table
 insert into rotation_assigned values('AAAAAAAAAAAAAAAA', '2017-04-07', '00000000');
--- second foreign key does not exist
+--  testing with second foreign key does not exist in Rotation Assigned table
 insert into rotation_assigned values('DISP17TU00000100', '2017-04-07', '99999999');
