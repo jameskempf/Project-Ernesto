da228ee8264f8f9765fb918bcab2404afd4f6b09
annv12321 <anna.pelletier@gordon.edu>
2017-03-21 23:18:07 -0400

    Update test_parker_assigned.sql

--- test/test_parker_assigned.sql
+++ test/test_parker_assigned.sql
@@ -1,6 +1,6 @@
--- duplicate primary key
+-- testing with duplicate primary key in Parker Assigned table
 insert into parker_assigned values('2017-04-03', '0000', '0100', '00000000');
--- first primary key does not exist
+-- testing with first primary key does not exist in Parker Assigned table
 insert into parker_assigned values('2000-01-01', '2300', '2400', '00000000');
--- second primary key does not exist
+-- testing with second primary key does not exist in Parker Assigned table
 insert into parker_assigned values('2017-04-03', '0000', '0100', '99999999');
