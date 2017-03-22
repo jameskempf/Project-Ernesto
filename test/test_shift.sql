78d831062a9fab03975e87e474c9dc640a8be9ea
annv12321 <anna.pelletier@gordon.edu>
2017-03-21 23:21:45 -0400

    Update test_shift.sql

--- test/test_shift.sql
+++ test/test_shift.sql
@@ -1,10 +1,10 @@
--- duplicate primary key
+--  testing with duplicate primary key in Shift table
 insert into shift values('DISP17MD00000100', 'Monday', '0000', '0100', 'Spring', 'F', '00000000');
--- foreign key does not exist
+--  testing with foreign key does not exist in Shift table
 insert into shift values('DISP17TU01000200', 'Tuesday', '0100', '0200', 'Fall', 'F', '99999999');
--- improper day name
+--  testing with improper day name in Shift table
 insert into shift values('DISP17TU01000200', 'not a dat', '0100', '0200', 'Fall', 'F', '00000000');
--- improper term name
+--  testing with improper term name in Shift table
 insert into shift values('DISP17TU01000200', 'Tuesday', '0100', '0200', 'not a term', 'F', '00000000');
--- improper boolean value
+--  testing with improper boolean value in Shift table
 insert into shift values('DISP17TU01000200', 'Tuesday', '0100', '0200', 'Fall', 'N', '00000000');
