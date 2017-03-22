e25802e47c2684df6d8a4649252a9c990d87f662
annv12321 <anna.pelletier@gordon.edu>
2017-03-21 22:15:34 -0400

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,4 +1,6 @@
 -- Queries for populating each table
+
+--Queries to fill in Position table
 insert into position values('Dispatcher', '12.00');
 insert into position values('Senior Dispatcher', '13.00');
 insert into position values('PEA', '13.00' );
@@ -7,6 +9,7 @@ insert into position values('Opener', '11.50');
 insert into position values('Closer', '12.00');
 insert into position values('Head Dispatcher', '14.00');
 
+--Queries to fill in Worker table
 insert into worker values('11223344', 'Audrey', 'Hepburn');
 insert into worker values('55667788', 'Sherlock', 'Holmes');
 insert into worker values('11335577', 'Bill', 'Cipher');
@@ -38,6 +41,8 @@ insert into worker values('11112222', 'John', 'Smith');
 insert into worker values('33334444', 'Jim', 'Gordon');
 insert into worker values('55556666', 'Bruce', 'Wayne');
 
+
+--Queries to fill Works table
 insert into works values('Dispatcher', '12345678');
 insert into works values('Senior Dispatcher', '11223344');
 insert into works values('Head Dispatcher', '55667788');
@@ -57,12 +62,15 @@ insert into works values('Dispatcher', '78912345');
 insert into works values('Parker', '89123456');
 insert into works values('Parker', '91234567');
 
+
+--Queries to fill Shift table
 insert into shift values('DISP17MD06001400', 'Monday', '0600', '1400', 'Spring', 'F', '12345678');
 insert into shift values('DISP17TU14002200', 'Tuesday', '1400', '2200', 'Spring', 'F', '55667788');
 insert into shift values('OPSP17WE06001400', 'Wednesday', '0600', '1400', 'Spring', 'F', '11335577');
 insert into shift values('PASP17TH06001400', 'Thursday', '0600', '1400', 'Spring', 'F', '22446688');
 insert into shift values('DISP17SU08001200', 'Sunday', '0800', '1200', 'Spring', 'T', NULL);
 
+--Queries to fill Shift Type table
 insert into shift_type values('DISP17MD06001400', 'Dispatcher');
 insert into shift_type values('DISP17MD06001400', 'Senior Dispatcher');
 insert into shift_type values('DISP17MD06001400', 'Head Dispatcher');
@@ -75,24 +83,28 @@ insert into shift_type values('DISP17SU08001200', 'Dispatcher');
 insert into shift_type values('DISP17SU08001200', 'Senior Dispatcher');
 insert into shift_type values('DISP17SU08001200', 'Head Dispatcher');
 
+--Queries to fill Rotation Assigned table
 insert into rotation_assigned values('DISP17SU08001200', '2017-04-07', '34567891');
 insert into rotation_assigned values('DISP17SU08001200', '2017-04-02', '45678912');
 insert into rotation_assigned values('DISP17SU08001200', '2017-03-19', '56789012');
 insert into rotation_assigned values('DISP17SU08001200', '2017-03-05', '67891234');
 insert into rotation_assigned values('DISP17SU08001200', '2017-03-26', '78912345');
 
+--Queries to fill Coverage table
 insert into coverage values('DISP17MD06001400', '2017-04-03', NULL, 'F');
 insert into coverage values('DISP17TU14002200', '2017-04-04', '12345678', 'T');
 insert into coverage values('OPSP17WE06001400', '2017-04-05', NULL, 'F');
 insert into coverage values('PASP17TH06001400', '2017-04-06', NULL, 'F');
 insert into coverage values('DISP17SU08001200', '2017-04-07', NULL, 'F');
 
+--Queries to fill Parker Shift table
 insert into parker_shift values('2017-04-03', '0800', '1200', '03');
 insert into parker_shift values('2017-04-10', '1200', '1600', '04');
 insert into parker_shift values('2017-04-17', '1600', '2000', '10');
 insert into parker_shift values('2017-04-24', '0800', '1200', '05');
 insert into parker_shift values('2017-03-27', '1200', '1400', '12');
 
+--Queries to fill Parker Assigned table
 insert into parker_assigned values('2017-04-03', '0800', '1200', '22446688');
 insert into parker_assigned values('2017-04-10', '1200', '1600', '44444444');
 insert into parker_assigned values('2017-04-17', '1600', '2000', '22446688');
