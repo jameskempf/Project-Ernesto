03b2b0c8172bcd8a17e153b4104f75425c4ff11f
annv12321 <anna.pelletier@gordon.edu>
2017-03-22 00:19:44 -0400

    Update test_parker_assigned.sql

--- test/test_parker_assigned.sql
+++ test/test_parker_assigned.sql
@@ -4,3 +4,7 @@ insert into parker_assigned values('2017-04-03', '0000', '0100', '00000000');
 insert into parker_assigned values('2000-01-01', '2300', '2400', '00000000');
 -- testing with second primary key does not exist in Parker Assigned table
 insert into parker_assigned values('2017-04-03', '0000', '0100', '99999999');
+-- testing the deleting cascade in Parker Assigned table (gid)
+select * from parker_assigned;
+delete * from worker where gid = '44444444';
+select * from parker_assigned;
