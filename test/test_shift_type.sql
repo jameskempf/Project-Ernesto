cbf68c97908ca6f99f06268556237de8753f6d3c
annv12321 <anna.pelletier@gordon.edu>
2017-03-21 23:22:44 -0400

    Update test_shift_type.sql

--- test/test_shift_type.sql
+++ test/test_shift_type.sql
@@ -1,6 +1,6 @@
--- duplicate primary key
+--  testing with duplicate primary key in Shift Type table
 insert into shift_type values('DISP17MD00000100', 'Dispatcher');
--- first foreign key does not exist
+--  testing with first foreign key does not exist in Shift Type table
 insert into shift_type values('AAAAAAAAAAAAAAAA', 'Dispatcher');
--- second foreign key does not exist
+--  testing with second foreign key does not exist in Shift Type table
 insert into shift_type values('DISP17MD00000100', 'not a pos_name');
